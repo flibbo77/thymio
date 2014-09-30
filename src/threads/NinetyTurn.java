315f93e2527c4c29fdb387210185353b8f15c63e
philipp <philipp.roehl@stud.uni-regensburg.de>
2014-09-30 22:46:51 +0200

    add all existing files to git repo

---
+++ src/threads/NinetyTurn.java
@@ -0,0 +1,63 @@
+package threads;
+
+import thymio.Thymio;
+
+public class NinetyTurn extends Thread {
+
+	int degr;
+	short speed;
+	Thymio thy;
+
+	public NinetyTurn(int degrees, short speed, Thymio myThymio) {
+		degr = degrees;
+		this.speed = speed;
+		thy = myThymio;
+	}
+
+	public void run() {
+		thy.rotate = true;
+		double orientation = thy.myPanel.myMap.getThymioOrientation();
+		thy.isDriving = true;
+		if (degr < 0)
+			speed *= -1;
+		thy.setVLeft(speed);
+		thy.setVRight((short) -speed);
+		while (Math.abs(Math.toDegrees(orientation
+				- thy.myPanel.myMap.getThymioOrientation())) < Math.abs(degr)) {
+
+			System.out.println("Rotation_state: "
+					+ Math.toDegrees(orientation
+							- thy.myPanel.myMap.getThymioOrientation()));
+			thy.updatePose(System.currentTimeMillis());
+		}
+		thy.setVLeft((short) 0);
+		thy.setVRight((short) 0);
+		thy.isDriving = false;
+		thy.updatePose(System.currentTimeMillis());
+
+		if(Math.toDegrees(orientation
+				- thy.myPanel.myMap.getThymioOrientation()) - degr > 2){
+			thy.isDriving = true;
+			speed = (short)-speed;
+			System.out.println("Rotation_state: "
+					+ Math.toDegrees(orientation
+							- thy.myPanel.myMap.getThymioOrientation()));
+			thy.setVLeft((short)(speed/5));
+			thy.setVRight((short) (-speed/5));
+			while(Math.toDegrees(orientation
+				- thy.myPanel.myMap.getThymioOrientation()) > degr){
+				System.out.println("Rotation_state: "
+						+ Math.toDegrees(orientation
+								- thy.myPanel.myMap.getThymioOrientation()));
+				thy.updatePose(System.currentTimeMillis());
+			}
+			thy.setVLeft((short) 0);
+			thy.setVRight((short) 0);
+			thy.isDriving = false;
+			thy.rotate = false;
+			thy.updatePose(System.currentTimeMillis());
+			
+		}
+	}
+
+}
